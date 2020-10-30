643d308301f9cfc596023fe4ac5067a34db1994d
SantiGonzalezLago <santiago.gonzalez.lago@gmail.com>
2020-10-30 21:51:16 +0100

    Exceptions

--- src/main/java/gal/teis/ejercicios/App.java
+++ src/main/java/gal/teis/ejercicios/App.java
@@ -1,13 +1,32 @@
 package gal.teis.ejercicios;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		exceptions();
+	}
+
+	public static void exceptions() {
+		int number;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("introduzca un numero");
+		try {
+			number = sc.nextInt();
+			if (number % 2 == 0) {
+				System.out.println("El número " + number + " es par.");
+			} else {
+				System.out.println("El número " + number + " es impar.");
+			}
+		} catch (Exception e) {
+			System.out.println("Ha habido un error en la introducción de datos");
+		} finally {
+			sc.close();
+		}
+
+	}
 }
