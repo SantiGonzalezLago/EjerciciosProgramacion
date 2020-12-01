01aa565a7a4225a3fb1cb217c6561f32ed6b62bc
SantiGonzalezLago <santiago.gonzalez.lago@gmail.com>
2020-12-01 17:36:26 +0100

    guessMonth() mejorado
    También funciona con número de mes y con cualquier combinación de
    mayúsculas y minúsculas

--- src/main/java/gal/teis/ejercicios/Unit2Arrays.java
+++ src/main/java/gal/teis/ejercicios/Unit2Arrays.java
@@ -10,37 +10,52 @@ import gal.teis.io.KeyboardScanner;
 public class Unit2Arrays {
 
 	public static void start() {
-//		guessMonth();
-		calculateDniLetter();
+		guessMonth();
+//		calculateDniLetter();
 	}
 
-	//Práctica 1
+	// Práctica 1
 	private static void guessMonth() {
-		final String[] MONTHS = {"enero" , "febrero" , "marzo" , "abril" , "mayo" , "junio" , "julio" , "agosto" , "septiembre", "octubre", "noviembre", "diciembre"};
-		String secretMonth = MONTHS[new Random().nextInt(MONTHS.length)];
+		final String[] MONTHS = { "enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto",
+				"septiembre", "octubre", "noviembre", "diciembre" };
+		int secretMonthNum = new Random().nextInt(MONTHS.length);
+		String secretMonth = MONTHS[secretMonthNum];
 		KeyboardScanner sc = new KeyboardScanner();
-		String month;
-		System.out.print("Adivine el mes secreto. Introduzca el nombre del mes en minúsculas: ");
-		month = sc.nextLine();
-		while (!month.equals(secretMonth)) {
-			System.out.print("No ha acertado. Intente adivinarlo introduciendo otro mes: ");
-			month = sc.nextLine();
-		}
-		System.out.println("¡Ha acertado!");
+		String month = "";
+		int monthNum;
+		boolean firstTime = true;
+		do {
+			if (firstTime) {
+				System.out.print("Adivine el mes secreto. Introduzca el nombre del mes o como número: ");
+				firstTime = false;
+			} else {
+				System.out.printf("No es %s. Intente adivinarlo introduciendo otro mes: ", month);
+			}
+			month = sc.nextLine().toLowerCase();
+			try {
+				monthNum = Integer.parseInt(month) - 1;
+				month = MONTHS[monthNum];
+			} catch (NumberFormatException | ArrayIndexOutOfBoundsException ex) {
+			}
+		} while (!month.equals(secretMonth));
+		System.out.printf("¡Ha acertado! ¡El mes era %s!%n", secretMonth);
 	}
 
-	//Práctica 2
+	// Práctica 2
 	private static void calculateDniLetter() {
-		final char[] LETTERS = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+		final char[] LETTERS = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q',
+				'V', 'H', 'L', 'C', 'K', 'E' };
 		int dni;
 		try {
-			dni = Integer.valueOf(JOptionPane.showInputDialog(null, "Introduzca su DNI (sin la letra, utilice solo dígitos)", "DNI", JOptionPane.QUESTION_MESSAGE));
+			dni = Integer.valueOf(JOptionPane.showInputDialog(null,
+					"Introduzca su DNI (sin la letra, utilice solo dígitos)", "DNI", JOptionPane.QUESTION_MESSAGE));
 		} catch (NumberFormatException ex) {
 			dni = -1;
 		}
 		if (dni >= 0 && dni <= 99999999) {
-			char letter = LETTERS[dni%23];
-			JOptionPane.showMessageDialog(null, String.format("%08d-%c", dni, letter), "Resultado", JOptionPane.INFORMATION_MESSAGE);
+			char letter = LETTERS[dni % 23];
+			JOptionPane.showMessageDialog(null, String.format("%08d-%c", dni, letter), "Resultado",
+					JOptionPane.INFORMATION_MESSAGE);
 		} else {
 			JOptionPane.showMessageDialog(null, "El DNI es erróneo", "Error", JOptionPane.ERROR_MESSAGE);
 		}
